c8bba75c19947f8a9a4f4ccd961deba98e3ee191
shavkatJuraboyev <shavkatjoraboyev0@gmail.com>
2025-06-19 17:48:13 +0500

    'yangilandi'

--- config/settings.py
+++ config/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zyxo$duf=kouv7^@*7cg1ykzm((l+-jsh)-1a+8bo+ff!#(a=9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "zamingarden.uz", "www.zamingarden.uz", "185.191.141.225"]
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
 
 
 # Application definition
